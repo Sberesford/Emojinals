94c2f7878c066ed37467b8e962dccf4355cf6d54
Apple <apple@apples-macbook-pro-50.local>
2019-07-26 08:38:26 -0700

    Completion of Iteration 1

--- actionsAndOutlets/ViewController.swift
+++ actionsAndOutlets/ViewController.swift
@@ -9,12 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    
+    
+    }
+    
+    @IBAction func showMessage(sender: UIButton){
+        //when user clicks tomato emoji the first message should say "Tuh-May-Toe"
+        //other message should say "Tuh-Mah-Toe"
+        
+        let alertController = UIAlertController (title: "Do you think it's pronounced", message: "Tuh-May-Toe", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "Clear", style: UIAlertAction.Style.default, handler: nil))
+        
+        present(alertController, animated: true, completion: nil)
+        
+        
     }
 
 
