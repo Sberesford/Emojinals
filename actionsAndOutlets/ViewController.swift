ba765be3c3ac9e89e2ca307dd3a4d59989138c27
Apple <apple@apples-macbook-pro-50.local>
2019-07-26 07:20:02 -0700

    Initial Commit

---
+++ actionsAndOutlets/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  actionsAndOutlets
+//
+//  Created by Apple on 7/25/19.
+//  Copyright © 2019 KWK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
